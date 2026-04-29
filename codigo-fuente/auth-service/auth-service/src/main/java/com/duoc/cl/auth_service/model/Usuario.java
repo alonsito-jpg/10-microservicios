06483c97069ac8a45ddc00ecf2909816ad46880a
Ubuntu <ubuntu@ip-172-31-34-232.ec2.internal>
2026-04-29 19:53:29 +0000

    Estructura inicial CSR de Auth-Service

---
+++ codigo-fuente/auth-service/auth-service/src/main/java/com/duoc/cl/auth_service/model/Usuario.java
@@ -0,0 +1,26 @@
+package com.duoc.cl.auth_service.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data   
+@NoArgsConstructor  
+@AllArgsConstructor
+@Entity
+
+public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+        
+    private String username;
+    private String password;
+    private String rol; 
+    
+}
